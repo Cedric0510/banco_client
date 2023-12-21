8e1d55fd78558836ec28ed2282098ace083acd84
Cédric <cedric.vanhove.82@gmail.com>
2023-12-21 11:42:59 +0100

    BAN-62/MyBudgetButton added

--- banco_client_react/src/components/TestPage.js
+++ banco_client_react/src/components/TestPage.js
@@ -1,13 +1,16 @@
 //import ici votre élément
-
 import NavBarTemplate from "./navBars/NavBarTemplate";
+import MyBudgetButton from "./buttons/MyBudgetButton";
+import ManageButtonBudget from "./buttons/ManageButtonBudget";
 
 function TestPage(){
 
-    return(
+    return (
         <>
         <h1>Page de test</h1>
         <NavBarTemplate />
+        <MyBudgetButton />
+        <ManageButtonBudget />
         <p>Testez ici votre élement</p>
         </>
     )
