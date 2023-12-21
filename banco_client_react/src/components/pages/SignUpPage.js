317dab9fb035daca9897d5e51c9f1a92eba99140
Ruah <bwb.e.ruah@fondespierre.com>
2023-12-21 16:53:43 +0100

    invitation sentence signup form & signup form added

--- banco_client_react/src/components/pages/SignUpPage.js
+++ banco_client_react/src/components/pages/SignUpPage.js
@@ -1,10 +1,12 @@
 import NavBarHome from "../navBars/NavBarHome";
+import SignUpForm from "../forms/FormulaireDinscription/SignUpForm";
 
 function SignUpPage(){
     return(
         <>
         <NavBarHome />
         <h1>Welcome Sign Up Page</h1>
+        <SignUpForm />
         </>
         
     )
